99977856e8541e08cc8f228eef55079aa5a312e1
Pedro Barreto <pedrosousabarreto@gmail.com>
2020-05-25 03:18:18 +0100

    removed dist and fixed gitignore

--- dist/shared/domain_abstractions/entity_factory.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by pedrosousabarreto@gmail.com on 22/May/2020.
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=entity_factory.js.map
