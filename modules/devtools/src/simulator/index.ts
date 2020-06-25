b59e495851736e95ccdc741c05c441b838947a9f
Miguel de Barros <miguel@debarros.me>
2020-06-25 12:07:52 +0200

    fixes for autocommitInternval & autocommitThreshold, also updated helm to support the configs

--- modules/devtools/src/simulator/index.ts
+++ modules/devtools/src/simulator/index.ts
@@ -77,8 +77,8 @@ Program.command('handler')
         consumer: (process.env.KAFKA_CONSUMER == null) ? KafkaInfraTypes.NODE_KAFKA : process.env.KAFKA_CONSUMER,
         producer: (process.env.KAFKA_PRODUCER == null) ? KafkaInfraTypes.NODE_KAFKA : process.env.KAFKA_PRODUCER,
         autocommit: (process.env.KAFKA_AUTO_COMMIT === 'true'),
-        autoCommitInterval: process.env.KAFKA_AUTO_COMMIT_INTERVAL,
-        autoCommitThreshold: process.env.KAFKA_AUTO_COMMIT_THRESHOLD
+        autoCommitInterval: (process.env.KAFKA_AUTO_COMMIT_INTERVAL != null && !isNaN(Number(process.env.KAFKA_AUTO_COMMIT_INTERVAL)) && process.env.KAFKA_AUTO_COMMIT_INTERVAL?.length > 0) ? Number.parseInt(process.env.KAFKA_AUTO_COMMIT_INTERVAL) : null,
+        autoCommitThreshold: (process.env.KAFKA_AUTO_COMMIT_THRESHOLD != null && !isNaN(Number(process.env.KAFKA_AUTO_COMMIT_THRESHOLD)) && process.env.KAFKA_AUTO_COMMIT_THRESHOLD?.length > 0) ? Number.parseInt(process.env.KAFKA_AUTO_COMMIT_THRESHOLD) : null
       }
     }
 
