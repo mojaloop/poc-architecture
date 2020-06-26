a451152a10af34a2befdbc501c70e7cfef7ad29e
Miguel de Barros <miguel@debarros.me>
2020-06-26 11:03:03 +0200

    fixes for compression support

--- modules/devtools/src/utilities/publisher.ts
+++ modules/devtools/src/utilities/publisher.ts
@@ -21,7 +21,11 @@ export const init = async (): Promise<void> => {
     appConfig = {
       kafka: {
         host: process.env.KAFKA_HOST ?? 'localhost:9092',
-        producer: (process.env.KAFKA_PRODUCER == null) ? KafkaInfraTypes.NODE_KAFKA : process.env.KAFKA_PRODUCER
+        producer: (process.env.KAFKA_PRODUCER == null) ? KafkaInfraTypes.NODE_KAFKA : process.env.KAFKA_PRODUCER,
+        autocommit: (process.env.KAFKA_AUTO_COMMIT === 'true'),
+        autoCommitInterval: (process.env.KAFKA_AUTO_COMMIT_INTERVAL != null && !isNaN(Number(process.env.KAFKA_AUTO_COMMIT_INTERVAL)) && process.env.KAFKA_AUTO_COMMIT_INTERVAL?.trim()?.length > 0) ? Number.parseInt(process.env.KAFKA_AUTO_COMMIT_INTERVAL) : null,
+        autoCommitThreshold: (process.env.KAFKA_AUTO_COMMIT_THRESHOLD != null && !isNaN(Number(process.env.KAFKA_AUTO_COMMIT_THRESHOLD)) && process.env.KAFKA_AUTO_COMMIT_THRESHOLD?.trim()?.length > 0) ? Number.parseInt(process.env.KAFKA_AUTO_COMMIT_THRESHOLD) : null,
+        gzipCompression: (process.env.KAFKAJS_PRODUCER_GZIP === 'true')
       },
       simulator: {
         host: process.env.SIMULATOR_HOST
