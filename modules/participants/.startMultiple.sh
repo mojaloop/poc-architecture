c030d20a83979b5de1e88d4b8ff797661fad8d2d
Miguel de Barros <miguel@debarros.me>
2020-07-08 12:59:58 +0200

    updates to helm script to handle different apiVersions

--- modules/participants/.startMultiple.sh
+++ modules/participants/.startMultiple.sh
@@ -14,3 +14,8 @@ PARTICIPANTS_API_PORT=3018 npm start &
 # PARTICIPANTS_API_PORT=3020 npm start &
 # PARTICIPANTS_API_PORT=3021 npm start &
 # PARTICIPANTS_API_PORT=3022 npm start &
+
+
+# MLIngressEvents
+# docker exec -it kafka sh -c "/opt/kafka_*/bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic TransferDomainEvents --partitions 2"
+# docker exec -it kafka sh -c "/opt/kafka_*/bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic MLIngressEvents --partitions 2"
