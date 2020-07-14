bffcf198980d48096a8516e64fb53ed5d1d38161
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2020-07-14 16:45:00 +0100

    changed all perfMeasures to mojaLogger

--- modules/devtools/src/utilities/perfMeasure.ts
+++ modules/devtools/src/utilities/perfMeasure.ts
@@ -191,7 +191,7 @@ function logRPS (): void {
   const avgFulfiled = Math.floor(fulfiledCounter / (elaspsed / 1000))
 
   // eslint-disable-next-line no-console
-  console.log(`\n *** ${counter} req/sec *** ${avg} avg ms *** ${evtMap.size} pending *** ${avgRequested}/${avgFulfiled} avg req/ful (all time) ***\n`)
+  logger.isInfoEnabled() && logger.info(`\n *** ${counter} req/sec *** ${avg} avg ms *** ${evtMap.size} pending *** ${avgRequested}/${avgFulfiled} avg req/ful (all time) ***\n`)
 
   if (buckets.has(lastSecond - 1)) {
     buckets.delete(lastSecond - 1)
@@ -322,12 +322,12 @@ start().catch((err) => {
 
 process.on('SIGINT', function () {
   // eslint-disable-next-line no-console
-  console.log('Ctrl-C... collecting pending...')
+  logger.isInfoEnabled() && logger.info('Ctrl-C... collecting pending...')
   const now = Date.now()
 
   evtMap.forEach((value, key) => {
     // eslint-disable-next-line no-console
-    console.log(`Pending transfer - ID: ${key} - Timestamp: ${value} - Age: ${now - value} ms`)
+    logger.isInfoEnabled() && logger.info(`Pending transfer - ID: ${key} - Timestamp: ${value} - Age: ${now - value} ms`)
   })
 
   process.exit(2)
